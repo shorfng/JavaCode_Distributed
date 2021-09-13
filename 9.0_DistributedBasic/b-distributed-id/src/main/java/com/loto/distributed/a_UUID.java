bfa4e05067f1e9a0c6cdc5d37ed8d70c64b79402
蓝田_Loto <shorfng@126.com>
2021-09-13 15:57:09 +0800

    分布式 ID 解决方案 - UUID

---
+++ 9.0_DistributedBasic/b-distributed-id/src/main/java/com/loto/distributed/a_UUID.java
@@ -0,0 +1,10 @@
+package com.loto.distributed;
+
+import java.util.UUID;
+
+public class a_UUID {
+    public static void main(String[] args) {
+        String uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
+    }
+}
