4f72d7d590c566f2a4797ff216eecd7c56d04ab4
蓝田_Loto <shorfng@126.com>
2021-09-27 13:30:10 +0800

    基于 Netty 的 WebSocket 开发网页版聊天室

---
+++ 9.2_Netty/netty6-eg-chatroom-websocket/src/main/java/com/loto/netty/controller/ChatController.java
@@ -0,0 +1,12 @@
+package com.loto.netty.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ChatController {
+    @RequestMapping("/")
+    public String chat() {
+        return "chat";
+    }
+}
