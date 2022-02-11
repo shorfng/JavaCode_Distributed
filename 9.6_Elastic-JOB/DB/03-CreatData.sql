822f4d49a60737b9bd823319b262a6ddc83e7032
蓝田_Loto <shorfng@126.com>
2022-02-11 21:36:09 +0800

    修改项目名称为 Java_10.10_ElasticSearch

--- 9.6_Elastic-JOB/DB/03-CreatData.sql
+++
@@ -1,5 +0,0 @@
-# 插入初始数据
-INSERT INTO `JavaWeb_9.6_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1001, 'TD', '男', '123456', '北京市', 'bachelor', '未归档');
-INSERT INTO `JavaWeb_9.6_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1002, 'CJ', '男', '111222', '北京市', 'master', '未归档');
-INSERT INTO `JavaWeb_9.6_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1003, 'loto', '男', '111222', '北京市', 'doctor', '未归档');
-
