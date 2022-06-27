60cb9b01c964e5178060a7a0fbf331b92e33c449
蓝田_Loto <shorfng@126.com>
2022-06-27 22:58:35 +0800

    更新数据库

--- Java_10.5_Elastic-JOB/DB/03-CreatData.sql
+++ Java_10.5_Elastic-JOB/DB/03-CreatData.sql
@@ -1,5 +1,5 @@
 # 插入初始数据
-INSERT INTO `JavaWeb_Java_10.5_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1001, 'TD', '男', '123456', '北京市', 'bachelor', '未归档');
-INSERT INTO `JavaWeb_Java_10.5_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1002, 'CJ', '男', '111222', '北京市', 'master', '未归档');
-INSERT INTO `JavaWeb_Java_10.5_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1003, 'loto', '男', '111222', '北京市', 'doctor', '未归档');
+INSERT INTO `Java_10.5_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1001, 'TD', '男', '123456', '北京市', 'bachelor', '未归档');
+INSERT INTO `Java_10.5_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1002, 'CJ', '男', '111222', '北京市', 'master', '未归档');
+INSERT INTO `Java_10.5_Elastic-JOB`.resume (id, name, sex, phone, address, education, state) VALUES (1003, 'loto', '男', '111222', '北京市', 'doctor', '未归档');
 
